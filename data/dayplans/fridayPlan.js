b9a6914914b3166a81f34a769135059a114d6c8b
linmingxing <linmingxing@xiaomi.com>
2016-07-16 13:45:07 +0800

    add friday plan.

---
+++ data/dayplans/fridayPlan.js
@@ -0,0 +1,11 @@
+'use strict';
+app.run(function(storage){
+
+  var fridayPlanData = {
+    "周五事项":{
+      "无":{}
+    }
+  };
+
+  storage.register("周五事项", fridayPlanData, true, "2016/7/15 10:00", "2016/7/15 23:00");
+});
