4dd67a535054af244d1a00604642475761e9f87e
linmingxing <linmingxing@xiaomi.com>
2016-07-25 14:10:23 +0800

    remove finished monday plan

--- data/dayplans/mondayPlan.js
+++ data/dayplans/mondayPlan.js
@@ -7,24 +7,14 @@ app.run(function(storage, dailyUtil){
         "IAuth错误更正": {_dl: "11:00", _f: true},
         "Staging流量完整切换": {_dl: "14:00", _f:true},
         "发送上线申请": {_dl: "15:00", _f: true},
-        "上线完成并切换流量": {_dl: "18:00"}
+        "上线完成并切换流量": {_dl: "18:00", _f: true}
       },
       "OAuth流量搜索": {
         "简单验证一天的OAuth数据": {_dl: "12:00", _f: 0.5},
         "发出每周的Job":{_dl: "13:30"}
       },
       "HTTPS支持开发": {
-        "逻辑开发": {
-          "Playlib-DataCenterUtil更改":{_f: true},
-          "ZK加入新配置":{_f: true},
-          "HostListUtil加入getHostList支持版本":{_f: true},
-          "上层调用": {_f: true},
-          _dl: "20:00",
-          _ac: true
-        },
         "测试": {
-          "添加单元测试": {_f: true},
-          "添加BVT测试": {_ac: true},
           "搭建Onebox测试环境": {}
         }
       }
