df304618766f6141bfb3195ae391bb471a12aefb
linmingxing <linmingxing@xiaomi.com>
2016-09-12 14:47:43 +0800

    remove finished jobs.

--- data/plans/dayPlan.js
+++ data/plans/dayPlan.js
@@ -5,31 +5,12 @@ app.run(function(storage, dailyUtil){
     "日事项":{
       "工作": {
         "ClientSDK": {
-          "背景了解": {
-            _ac: true,
-            _dl: "16:00",
-            "antlr语法学习": {_dl: "12:00", _f: true},
-            "twirl模板框架使用学习": {_dl: "15:00", _f: true}
-          },
-          "当前工作状态接手": {_f: 0.6}
         },
-        "剩余服务更新推动": {
-
-        }
+        "剩余服务更新推动": {}
       },
       "学习": {
-        "ExecutorService实现": {
-          "测试": {_f: 0.8},
-          "libcap更改": {_f: true}
-        },
-        "Tomcat长连接作用机制": {
-          "tomcat-native-poll机制研究": {_f: true},
-          _ac: true
-        },
         "Thrift框架研究": {
-          "Thrift基础部分": {_ac: true, _f: true},
-          "Swift解释器部分": {},
-          "Thrift服务发现部分": {}
+          "Swift解释器部分": {}
         }
       }
     }
--- data/plans/weekPlan.js
+++ data/plans/weekPlan.js
@@ -5,38 +5,16 @@ app.run(function(storage, weeklyUtil){
     "周计划":{
       "工作": {
         "ClientSDK工作": {
-          "背景了解": {_ac: true, _dl: "Wed 20:00", _f: true},
           "项目细化与时间估计": {}
         },
         "其他服务上线": {
-          "Https支持开启": {_ac: true, _dl: "Tue 18:00", _f: true},
           "其他服务上线": {_ac: true, _dl: "Web 12:00", _f: true}
         }
       },
       "学习": {
-        "Apache Tomcat": {
-          "时序图整理": {
-            "协议层整理": {_f: true},
-            "容器层整理": {_f: true},
-            "应用层整理": {_f: true}
-          },
-          "Lifecycle机制研究": {_f: true},
-          "特殊情况处理": {
-            "Https支持": {_f: true},
-            "长连接支持学习": {_f: true}
-          },
-          _dl: "Wed 20:00"
-        },
         "ExecutorService完成": {
           "Job正常执行": {_ac: true, _f: 0.6},
-          "Scala的Future/Promise机制探究": {
-            _f: true
-          },
           "总结Thrift内部的一些高级的内容": {
-            "服务启动": {},
-            "利用Factory等对Thrift服务进行定制，达到自动发现等功能": {
-            },
-            "Thrift的序列化框架": {_ac: true}
           },
           _dl: "Tue 22:00"
         },
