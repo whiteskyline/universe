5a5b917205b1ffb17ce45941beae393a4c026c84
linmingxing <linmingxing@xiaomi.com>
2016-10-06 01:33:10 +0800

    all bugs fixed.

--- js/controller/parentPlanPanel.js
+++ js/controller/parentPlanPanel.js
@@ -7,14 +7,14 @@
 
  app.controller("parentPlanPanelCtrl", function($scope, bus, storage){
 
-   function collectNodeInfo(data, results, func, trans) {
+   function collectNodeInfo(data, results, func, trans, parent) {
      if (func(data)) {
-       results.push(trans(data))
+       results.push(trans(data, parent))
      }
 
      if (typeof(data.children) !== "undefined") {
        data.children.forEach(function(ele){
-         collectNodeInfo(ele, results, func, trans)
+         collectNodeInfo(ele, results, func, trans, data)
        })
      }
 
@@ -32,10 +32,18 @@
      }
      var parentName = parentLevelData.key
 
-     var parentPlan = transformDataFormat(parentName, parentLevelData.data[parentName])
+    //  var parentPlan = transformDataFormat(parentName, parentLevelData.data[parentName])
+    var parentPlan = parentLevelData.data
 
-     var trans = function(ele) {
-        return ele
+     var trans = function(ele, parent) {
+       var newEle = angular.copy(ele)
+       newEle.children = null
+       if (typeof(parent) === "undefined") {
+         return newEle
+       } else {
+         newEle.name = newEle.name + ":" + parent.name
+         return newEle
+       }
      }
 
      var resultNode = {"name": "父层级计划概览", unfinishedJobs:[], finishedJobs:[], withDeadlines:[], }
@@ -46,6 +54,19 @@
      collectNodeInfo(parentPlan, resultNode.finishedJobs, function(ele){
        return ele.detail.finished >= 1
      },trans)
+
+     resultNode.unfinishedJobs = resultNode.unfinishedJobs.sort(function(a, b) {
+       if (typeof(a.detail.deadline) === "undefined") {
+         return 1;
+       }
+
+       if (typeof(b.detail.deadline) === "undefined" ) {
+         return -1;
+       }
+
+       return new Date(a.detail.deadline) - new Date(b.detail.deadline)
+
+     })
 
      $scope.node = resultNode
    });
