1aadf2ce5c96f6dc895dd8dac077ca90d23805ec
linmingxing <linmingxing@xiaomi.com>
2016-10-06 01:53:28 +0800

    update plan

--- data/plans/dayPlan.js
+++ data/plans/dayPlan.js
@@ -5,23 +5,14 @@ app.run(function(storage, dailyUtil){
     "日事项":{
       "工作": {
         "ClientSDK": {
-          "Android依赖包处理": {_f: true},
-          "Android请求重写": {_f: true},
           "修改Wifi-SDK使用重写包": {},
-          "定义Shared的数据结构": {_f: true},
-          "定义Wifi相关的派生类": {_f: true},
-          "定义返回相关的Class": {_f: true},
-          "Thrift序列化问题解决": {_f: true},
-          "解决UserIdHolder的异常Exception问题": {_f: true}
-        },
-        "剩余服务更新推动": {
-          _dl: "23:00"
-        },
-        "mt-bug解决": {
-          _f: true
         }
       },
       "学习": {
+        "ExecutorService更新版本": {_dl: "10:00"},
+        "Redis两章": {_dl: "12:00"},
+        "SnowFlake服务的总结": {_dl: "14:00"},
+        "机器学习第一章完成": {_dl: "18:00"}
       }
     }
   };
